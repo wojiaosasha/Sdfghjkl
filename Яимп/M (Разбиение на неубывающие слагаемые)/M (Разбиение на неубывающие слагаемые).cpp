2715f294fb1e427c68ea4e07c093c06ca52f320a
wojiaosasha <sasha.79051@gmail.com>
2020-07-02 17:38:15 +1000

    +

---
+++ Яимп/M (Разбиение на неубывающие слагаемые)/M (Разбиение на неубывающие слагаемые).cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> a;
+
+void sep(int k, int m, int n) {
+	if (n == 0) 
+	{
+		for (int i = 0; i < k; cout << a[i] << ' ', i++);
+		cout << endl;
+	}
+	else 
+	{
+		for (int i = n; i >= m; i--) 
+		{
+			a[k] = i;
+			sep(k + 1, i, n - i);
+		}
+	}
+}
+
+int main() {
+	int n;
+	cin >> n;
+	a.resize(n+10);
+	sep(0, 1, n);
+}
